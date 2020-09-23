25a5b25bf3976efd6063eac5ea9237d2c4f709f7
Oleksandr Krokhin <alexander@krohin.com>
2020-09-23 15:57:50 +0300

    fiz prefix

--- options.php
+++ options.php
@@ -1,13 +1,13 @@
 <?php
 
-function add_settings_page()
+function iai_add_settings_page()
 {
-    add_options_page('Init Affiliate ID', 'Init Affiliate ID', 'manage_options', 'wp-init-affiliate-id', 'render_plugin_settings_page');
+    add_options_page('Init Affiliate ID', 'Init Affiliate ID', 'manage_options', 'wp-init-affiliate-id', 'iai_render_plugin_settings_page');
 }
 
-add_action('admin_menu', 'add_settings_page');
+add_action('admin_menu', 'iai_add_settings_page');
 
-function render_plugin_settings_page()
+function iai_render_plugin_settings_page()
 {
     ?>
     <h2>Init Affiliate ID Settings</h2>
@@ -20,7 +20,7 @@ function render_plugin_settings_page()
     <?php
 }
 
-function register_settings()
+function iai_register_settings()
 {
     register_setting('wp_init_affiliate_id_options', 'wp_init_affiliate_id_options', 'wp_init_affiliate_id_options_validate');
     add_settings_section('api_settings', 'ID Settings', 'wp_init_affiliate_id_section_text', 'wp_init_affiliate_id');
@@ -30,7 +30,7 @@ function register_settings()
 
 }
 
-add_action('admin_init', 'register_settings');
+add_action('admin_init', 'iai_register_settings');
 
 function wp_init_affiliate_id_options_validate($input)
 {
