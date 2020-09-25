3a99c21529d0c728fad46ed062120887e88ac2d5
Oleksandr Krokhin <alexander@krohin.com>
2020-09-25 13:33:46 +0300

    fix code

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 /**
  * Plugin Name: Init Affiliate ID
- * Version: 1.0.1
+ * Version: 1.0.4
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
  * Plugin URI: https://github.com/uleytech/wp-init-affiliate-id
  * Description: Provides functionality for WordPress.
  * Author: Oleksandr Krokhin
+ * Author URI: https://www.krohin.com
  * License: MIT
  */
 
