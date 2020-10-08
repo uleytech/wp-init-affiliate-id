0c39a8a87c5715eafa7bbb0acaad5c05f0ddb1bd
Oleksandr Krokhin <alexander@krohin.com>
2020-10-08 13:42:49 +0300

    add local 3

--- options.php
+++ options.php
@@ -47,7 +47,7 @@ function wp_init_affiliate_id_options_validate($input)
 
 function wp_init_affiliate_id_section_text()
 {
-    echo '<p>' . _e('Here you can set all the options for using the Post Affiliate Pro', 'wp-init-affiliate-id') . '</p>';
+    echo '<p>' . __('Here you can set all the options for using the Post Affiliate Pro', 'wp-init-affiliate-id') . '</p>';
 }
 
 function wp_init_affiliate_id_setting_aff_id()
@@ -66,5 +66,5 @@ function wp_init_affiliate_id_setting_expired_at()
 {
     $options = get_option('wp_init_affiliate_id_options');
     echo "<input id='wp_init_affiliate_id_setting_expired_at' name='wp_init_affiliate_id_options[expired_at]' type='number' value='" . esc_attr($options['expired_at']) . "' />
-        <span>" . _e('day(s)', 'wp-init-affiliate-id') . "</span>";
+        <span>" . __('day(s)', 'wp-init-affiliate-id') . "</span>";
 }
