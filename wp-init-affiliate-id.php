7a2cb4c6e914d3b5916b154e51e1e4fc42bb7aa7
Oleksandr Krokhin <alexander@krohin.com>
2020-10-13 12:17:33 +0300

    add langs

--- wp-init-affiliate-id.php
+++ wp-init-affiliate-id.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * Plugin Name: Init Affiliate ID
- * Version: 1.0.9
+ * Version: 1.0.12
  * Requires at least: 5.2
  * Requires PHP: 7.2
  * Plugin URI: https://github.com/uleytech/wp-init-affiliate-id
- * Description: Provides functionality for WordPress.
+ * Description: Provides functionality with Affiliate for WordPress.
  * Text Domain: wp-init-affiliate-id
+ * Domain Path: /languages/
  * Author: Oleksandr Krokhin
  * Author URI: https://www.krohin.com
  * License: MIT
@@ -18,8 +19,8 @@ function init_aff_cookie()
 {
     $refId = $_GET['aid'] ?? null;
     $options = get_option('wp_init_affiliate_id_options');
-    $affId = esc_attr($options['aff_id']) ?? 65514;
-    $expiredAt = esc_attr($options['expired_at']) ?? 30;
+    $affId = esc_attr($options['aff_id']);
+    $expiredAt = esc_attr($options['expired_at']);
     if (!isset($_COOKIE['aid'])) {
         $cid = $refId ?? $affId;
         setcookie('aid', $cid, time() + 60 * 60 * 24 * $expiredAt, COOKIEPATH, COOKIE_DOMAIN, false);
